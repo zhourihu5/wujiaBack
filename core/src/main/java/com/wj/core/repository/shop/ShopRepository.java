4a2dd2c91f127cd864ac68cebf6ca7f412864e5c
孙海洋 <sunhaiyang@efeiyi.com>
2019-08-22 20:40:10 +0800

    bug

--- core/src/main/java/com/wj/core/repository/shop/ShopRepository.java
+++ core/src/main/java/com/wj/core/repository/shop/ShopRepository.java
@@ -2,6 +2,7 @@ package com.wj.core.repository.shop;
 
 import com.wj.core.entity.shop.Shop;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ShopRepository extends JpaRepository<Shop, Integer> {
+public interface ShopRepository extends JpaRepository<Shop, Integer>, JpaSpecificationExecutor<Shop> {
 }
--- core/src/main/java/com/wj/core/service/shop/ShopService.java
+++ core/src/main/java/com/wj/core/service/shop/ShopService.java
@@ -1,16 +1,55 @@
 package com.wj.core.service.shop;
 
+import com.google.common.collect.Lists;
+import com.wj.core.entity.shop.Shop;
 import com.wj.core.repository.shop.ShopAddressRepository;
 import com.wj.core.repository.shop.ShopRepository;
+import com.wj.core.util.time.ClockUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.criteria.Predicate;
+import java.util.List;
 
 @Service
 public class ShopService {
 
     @Autowired
     private ShopRepository shopRepository;
-    @Autowired
-    private ShopAddressRepository shopAddressRepository;
+
+    public void save(Shop shop) {
+        if (shop.getId() == null) {
+            shop.setCreateDate(ClockUtil.currentDate());
+            shop.setCode(String.valueOf(ClockUtil.currentTimeMillis()));
+        }
+        shopRepository.save(shop);
+    }
+
+
+    public Page<Shop> getList(Integer pageNum, Integer pageSize, String shopName) {
+        Specification specification = (Specification) (root, criteriaQuery, criteriaBuilder) -> {
+            List<Predicate> predicates = Lists.newArrayList();
+            if (StringUtils.isNotBlank(shopName)) {
+
+            }
+            return criteriaBuilder.and(predicates.toArray(new Predicate[predicates.size()]));
+        };
+        if (pageNum == null) {
+            pageNum = 1;
+        }
+        if (pageSize == null) {
+            pageSize = 10;
+        }
+        Pageable page = PageRequest.of(pageNum - 1, pageSize, Sort.Direction.DESC, "id");
+        Page<Shop> pageShop = shopRepository.findAll(specification, page);
+        return pageShop;
+    }
+
 
 }
