92814cbc9db78c2e50dd3800a7610304686c47c6
thz <624941425@qq.com>
2019-09-02 14:33:25 +0800

    a

--- core/src/main/java/com/wj/core/service/activity/ActivityService.java
+++ core/src/main/java/com/wj/core/service/activity/ActivityService.java
@@ -267,20 +267,20 @@ public class ActivityService {
         Activity activity = activityRepository.findByActivityId(activityId);
         activity.setCommodity(commodityRepository.findByCommodityId(activity.getCommodityId()));
         Integer count = orderInfoRepository.findCountByActivityId(activityId);
-        String[] rules = activity.getSaleRules().split(",");
+//        String[] rules = activity.getSaleRules().split(",");
         Integer amount = 0;
-        for (int i = 1; i < rules.length; i++) {
-            Integer number0 = Integer.valueOf(rules[i-1].substring(0, rules[i-1].indexOf("|")));
-            Integer number = Integer.valueOf(rules[i].substring(0, rules[i].indexOf("|")));//截取|之前的字符串
-            Integer money0 = Integer.valueOf(rules[i-1].substring(rules[i-1].lastIndexOf("|") + 1));
-            Integer money = Integer.valueOf(rules[i].substring(rules[i].lastIndexOf("|") + 1));
-            System.out.println(number0 + "---" + money0);
-            System.out.println(number + "---" + money);
-            if (count >= number0 && count < number ) {
-                amount = money0;
-                break;
-            }
-        }
+//        for (int i = 1; i < rules.length; i++) {
+//            Integer number0 = Integer.valueOf(rules[i-1].substring(0, rules[i-1].indexOf("|")));
+//            Integer number = Integer.valueOf(rules[i].substring(0, rules[i].indexOf("|")));//截取|之前的字符串
+//            Integer money0 = Integer.valueOf(rules[i-1].substring(rules[i-1].lastIndexOf("|") + 1));
+//            Integer money = Integer.valueOf(rules[i].substring(rules[i].lastIndexOf("|") + 1));
+//            System.out.println(number0 + "---" + money0);
+//            System.out.println(number + "---" + money);
+//            if (count >= number0 && count < number ) {
+//                amount = money0;
+//                break;
+//            }
+//        }
         BigDecimal num = new BigDecimal(amount);
         BigDecimal payMoney = activity.getPrice().subtract(num);
         if (payMoney.doubleValue() <= 0) {
